73a6b87d6f141c6e2027dfff46a665a62c6aab9b
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-03-26 12:27:06 -0700

    added input and functions

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import { useState } from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap/Container';
+import { v4 as uuidv4 } from 'uuid';
 
-function Board(props) {
+class Task {
+  constructor(id, content) {
+    //constructor is function not a map
+    this.id = id;
+    this.content = content;
+  }
+}
 
+function Column(props) {
+
+  //each task should generate its own div tag n id to seperate 
+  const toDisplay = props.tasks.map(task => <div onClick='' key={task.id}> {task.content}</div>)
+  // props.tasks is a list of strings
+  // task is a string
+  // 
 
   return (
     <div>
+      {toDisplay}
     </div>
   )
 }
-
 
 function App() {
   //make 4 lists to store different stages 
@@ -21,42 +35,39 @@ function App() {
   // delete a task 
   // move a task 
 
-  const [task, setTask] = useState(null)
-  const [newTask, setNewTask] = useState(null)
+  //task string
+
+  //list of strings
+  const [tasks, setTasks] = useState([])
+
+  //planned [] of tasks (objs)
+  //inProgress [] 
+  //testing []
+  //done []
   const [planned, setPlanned] = useState([])
   const [inProgress, setinProgress] = useState([])
   const [testing, setTesting] = useState([])
   const [done, getDone] = useState([])
 
-
-
-  function onChangeAddTask(e) {
-    setTask(e.target.value)
-  }
-
-  console.log(task, 'task')
-
   function keyPressed(e) {
+
     if (e.key === "Enter") {
-      setPlanned([...planned, task])
+      let tempTask = new Task(uuidv4(), e.target.value)
+      setTasks([...tasks, tempTask])
+
     }
-    //each task should generate its own div tag to seperate 
   }
-
-  console.log(planned, 'planned')
-
+  console.log(tasks)
 
   return (
     <div className="App">
       Kanban
       <div className="container">
-        {planned}
-
 
       </div>
 
-      <input name="test" onKeyPress={keyPressed} onChange={onChangeAddTask} />
-      <Board task={task} />
+      <input name="test" onKeyPress={keyPressed} />
+      <Column tasks={tasks} />
     </div>
   );
 }
