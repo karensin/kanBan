2113bca0d5762e217263da7ea9cb182e636c76fa
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-04-14 14:57:49 -0700

    idk man

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { useState } from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap/Container';
+import { Container, Row, Col } from 'react-bootstrap';
 import { v4 as uuidv4 } from 'uuid';
+import { Card, Icon, Image, Button, Feed } from 'semantic-ui-react';
+
 //make 4 lists to store different stages 
 // 1. planned 2. in progress 3. testing 4. done  
 // create a task
@@ -28,23 +30,37 @@ class Stage {
 }
 
 function Column(props) {
+  let stagesNameList = ['Planned', 'In Progress', 'Testing', 'Completed']
+  let stagesName = stagesNameList[props.stageID]
 
   // (blah) => doSomething()
   // function noName(blah) {doSomething()}
   //each task should generate its own div tag n id to seperate 
   const toDisplay = props.tasks.map(task =>
-    <div>
-      {props.stageID}
-      <div onClick={e => props.onClickMoveRight(task.id, props.stageID)} key={task.id}>  right{task.content}  </div>
-      <button onClick={e => props.onClickDelete(task.id, props.stageID)} > </button>
-      <button onClick={e => props.onClickMoveLeft(task.id, props.stageID)}> Left</button>
-    </div>)
+
+    < div >
+      <Card.Description>
+        <strong>{task.content}</strong>
+      </Card.Description>
+      <Button color='green' onClick={e => props.onClickMoveLeft(task.id, props.stageID)}> Left
+      </Button>
+      <Button color='red' onClick={e => props.onClickDelete(task.id, props.stageID)} > Delete</Button>
+      < Button color='blue' onClick={e => props.onClickMoveRight(task.id, props.stageID)
+      } key={task.id} > Right
+      </Button >
+
+
+    </div >)
   // props.tasks is a list of strings
   // task is a string
 
   return (
+
     <div className="column">
-      {toDisplay}
+      <Card>
+        <Card.Header>  {stagesName}</Card.Header>
+        <Card.Content>  {toDisplay} </Card.Content>
+      </Card>
     </div>
   )
 }
@@ -52,7 +68,7 @@ function Column(props) {
 function App() {
   // const [tasks, setTasks] = useState([])
   // const [planned, setPlanned] = useState([])   //planned [] of tasks (objs)
-  // const [inProgress, setinProgress] = useState([])  //inProgress [] 
+  // const [inProgress, setinProgress] = useState([])  //inProgress []
   // const [testing, setTesting] = useState([])  //testing []
   // const [done, setDone] = useState([])   //done []
 
@@ -62,7 +78,9 @@ function App() {
   function keyPressed(e) {
 
     if (e.key === "Enter") {
+      console.log('fired', e.key)
       let tempTask = new Task(uuidv4(), e.target.value)
+      console.log(tempTask)
       // setPlanned([...planned, tempTask])
       //  let tempStages=  stages['planned'].push(tempTask)
       let tempStages = { ...stages }
@@ -91,18 +109,17 @@ function App() {
     stageID = parseInt(stageID)
 
     console.log(stageID, stageID + 1)
-    //add task to the right - in progress 
+    //add task to the right - in progress
     let taskClicked
-    //  [] of tasks inside the stage 
+    //  [] of tasks inside the stage
     let prevStage = stages[stageID]
     let nextStage = stages[stageID + 1]
-    //return nextStage with new task 
+    //return nextStage with new task
     prevStage.map((task) => {
       if (taskId === task.id) {
         taskClicked = task
       }
     })
-
 
     //return new list without the task clicked
     let filteredOldTaskList = prevStage.filter((task) => {
@@ -112,19 +129,16 @@ function App() {
     let tempStages = { ...stages }
     tempStages[stageID] = filteredOldTaskList
     // setinProgress([...inProgress, taskClicked])
-    //todo:  add to new list***** 
+    //todo:  add to new list*****
 
     tempStages[stageID + 1] = [...tempStages[stageID + 1], taskClicked]
     // tempStages[stageID + 1] = taskClicked
 
-
-
     setStages(tempStages)
-
 
     // setStages(tempStages)
 
-    //remove that task from planned 
+    //remove that task from planned
   }
 
   function onClickMoveLeft(taskId, stageID) {
@@ -132,12 +146,12 @@ function App() {
     stageID = parseInt(stageID)
 
     console.log(stageID, stageID + 1)
-    //add task to the right - in progress 
+    //add task to the right - in progress
     let taskClicked
-    //  [] of tasks inside the stage 
+    //  [] of tasks inside the stage
     let prevStage = stages[stageID]
     let nextStage = stages[stageID - 1]
-    //return nextStage with new task 
+    //return nextStage with new task
     prevStage.map((task) => {
       if (taskId === task.id) {
         taskClicked = task
@@ -153,7 +167,7 @@ function App() {
     let tempStages = { ...stages }
     tempStages[stageID] = filteredOldTaskList
     // setinProgress([...inProgress, taskClicked])
-    //todo:  add to new list***** 
+    //todo:  add to new list*****
 
     tempStages[stageID - 1] = [...tempStages[stageID - 1], taskClicked]
     // tempStages[stageID + 1] = taskClicked
@@ -165,7 +179,7 @@ function App() {
 
     // setStages(tempStages)
 
-    //remove that task from planned 
+    //remove that task from planned
 
   }
 
@@ -174,22 +188,37 @@ function App() {
     <div className="App">
       Kanban
       <div className="container"></div>
-      <input name="test" onKeyPress={keyPressed} />
-      {/* passing a variable of type function */}
-
-      < Column tasks={stages[0]} stageID='0' onClickMoveLeft={onClickMoveLeft} onClickDelete={onClickDelete} onClickMoveRight={onClickMoveRight} >
-        planned
+      <input type='text/javascript' name="test" onKeyPress={keyPressed} />
+      <Container className="d-flex align-items-stretch">
+        <Card.Group> </Card.Group>
+        <Card.Group>
+          <Card fluid color='red'>
+            < Column tasks={stages[0]} stageID='0' onClickMoveLeft={onClickMoveLeft} onClickDelete={onClickDelete} onClickMoveRight={onClickMoveRight} >
+            </Column>
+          </Card>
+        </Card.Group>
+        <Card.Group>
+          <Card fluid color='green'>
+            < Column tasks={stages[1]} stageID='1' onClickMoveLeft={onClickMoveLeft} onClickDelete={onClickDelete} onClickMoveRight={onClickMoveRight} >
+              In progress
       </Column>
-
-      < Column tasks={stages[1]} stageID='1' onClickMoveLeft={onClickMoveLeft} onClickDelete={onClickDelete} onClickMoveRight={onClickMoveRight} >
-        In progress
+          </Card>
+        </Card.Group>
+        <Card.Group>
+          <Card fluid color='blue' >
+            < Column tasks={stages[2]} stageID='2' onClickMoveLeft={onClickMoveLeft} onClickDelete={onClickDelete} onClickMoveRight={onClickMoveRight}>
+              Testing
       </Column>
-      < Column tasks={stages[2]} stageID='2' onClickMoveLeft={onClickMoveLeft} onClickDelete={onClickDelete} onClickMoveRight={onClickMoveRight}>
-        Testing
+          </Card>
+        </Card.Group>
+        <Card.Group>
+          <Card fluid color='grey' >
+            < Column tasks={stages[3]} stageID='3' onClickMoveLeft={onClickMoveLeft} onClickDelete={onClickDelete} onClickMoveRight={onClickMoveRight}>
+              Done
       </Column>
-      < Column tasks={stages[3]} stageID='done' onClickMoveLeft={onClickMoveLeft} onClickDelete={onClickDelete} onClickMoveRight={onClickMoveRight}>
-        Done
-      </Column>
+          </Card>
+        </Card.Group>
+      </Container>
     </div>
   );
 }
